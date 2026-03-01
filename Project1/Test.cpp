9a042f20bf35b5fdc4e1e9593a9a2b9ae6e7e3cf
Concorde0 <1462913972@qq.com>
2026-03-01 13:35:16 +0800

    update

--- Project1/Test.cpp
+++ Project1/Test.cpp
@@ -15,6 +15,7 @@ void processInput(GLFWwindow* window) {
 }
 
 int main() {
+    // 实例化 GLFW 窗口
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -31,14 +32,19 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 
+
+    // 渲染循环(Render Loop)
 	while (!glfwWindowShouldClose(window)) {
      
 
         processInput(window);
 
+        // 渲染指令
+        // glClearColor函数是一个状态设置函数，而glClear函数则是一个状态使用的函数，它使用了当前的状态来获取应该清除为的颜色。
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // 检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
